6dda0925d24e70b564e2f758c8b84b5c4aaa9b77
Richard Marques <richard@seberino.com.br>
2022-03-22 23:40:19 -0300

    update the database.sql DDL

---
+++ src/main/resources/base.sql
@@ -0,0 +1,1 @@
+create table cliente (cpf decimal(11) not null primary key, nome varchar(50), mae varchar(50), logradouro varchar(35), numero int, complemento varchar(35), cep char(9), cidade varchar(35), uf char(2), nasc date);
